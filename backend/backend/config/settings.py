addc3c4d60ef05e47d35876695dc7d9d9cc293e4
Jayesh Ekanth Sutar <81569410+akajayesh@users.noreply.github.com>
2026-05-12 22:21:35 +0530

    Migrations

--- backend/backend/config/settings.py
+++ backend/backend/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url, os 
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -88,10 +89,12 @@ DATABASES = {
         'PORT': '5433',
     }
 }'''
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
-    )
+    "default": dj_database_url.parse(DATABASE_URL)
 }
 
 
